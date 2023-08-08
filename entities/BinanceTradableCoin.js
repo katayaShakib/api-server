a4ea65df9a7a30cf1ba3fbb509e97abea239c937
Shakib KATAYA <kataya.shakib@gmail.com>
2023-08-08 03:39:06 +0300

    Create entity models

---
+++ entities/BinanceTradableCoin.js
@@ -0,0 +1,8 @@
+class BinanceTradableCoin {
+  constructor(symbol, averagePrice) {
+    this.symbol = symbol;
+    this.averagePrice = averagePrice;
+  }
+}
+
+module.exports = BinanceTradableCoin;
---
+++ entities/EthereumTransaction.js
@@ -0,0 +1,17 @@
+class EthereumTransaction {
+  constructor(
+    transactionHash,
+    senderAddress,
+    receiverAddress,
+    amountTransferred,
+    blockNumber
+  ) {
+    this.transactionHash = transactionHash;
+    this.senderAddress = senderAddress;
+    this.receiverAddress = receiverAddress;
+    this.amountTransferred = amountTransferred;
+    this.blockNumber = blockNumber;
+  }
+}
+
+module.exports = EthereumTransaction;
