8d0cfff35d9f7e312b4c3b3ec6e46db131a9a1dc
Shakib KATAYA <kataya.shakib@gmail.com>
2023-08-08 21:48:16 +0300

    Add swagger annotations for endpoints

--- controllers/ccxtController.js
+++ controllers/ccxtController.js
@@ -1,27 +1,57 @@
-const express = require('express');
-const ccxtService = require('../services/ccxtService');
+/**
+ * @swagger
+ * tags:
+ *   name: CCXT
+ *   description: CCXT API
+ */
+
+const express = require("express");
+const ccxtService = require("../services/ccxtService");
 
 const router = express.Router();
 
-// Route to get the list of tradable coins on Binance
-router.get('/tradableCoins', async (req, res) => {
-  try {
-    const tradableCoins = await ccxtService.getTradableCoins();
-    res.json(tradableCoins);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+/**
+ * @swagger
+ * /ccxt/tradableCoins:
+ *   get:
+ *     summary: Get the list of tradable coins on Binance
+ *     tags: [CCXT]
+ *     responses:
+ *       200:
+ *         description: Array of tradable coin objects containing the coins' symbol
+ *         content:
+ *           application/json:
+ *             example:
+ *               - symbol:BTC
+ *               - symbol:ETH
+ *       500:
+ *         description: Error occurred while fetching tradable coins
+ */
+router.get("/tradableCoins", async (req, res) => {
+  const tradableCoins = await ccxtService.getTradableCoins();
+  res.json(tradableCoins);
 });
 
-// Route to get the average price of a specific coin
-router.get('/tradableCoins/averagePrices', async (req, res) => {
-  const { coinSymbol } = req.params;
-  try {
-    const averagePrice = await ccxtService.getAveragePrice(coinSymbol);
-    res.json({ coinSymbol, averagePrice });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+/**
+ * @swagger
+ * /ccxt/tradableCoins/averagePrices:
+ *   get:
+ *     summary: Get the list of tradable coins on Binance with their average prices
+ *     tags: [CCXT]
+ *     responses:
+ *       200:
+ *         description: Array of tradable coin objects containing the coins' symbol and average price
+ *         content:
+ *           application/json:
+ *             example:
+ *               - symbol:BTC/USDT averagePrice:40000.0
+ *               - symbol:ETH/USDT averagePrice:2000.0
+ *       500:
+ *         description: Error occurred while fetching the average price
+ */
+router.get("/tradableCoins/averagePrices", async (req, res) => {
+  const averagePrice = await ccxtService.getAveragePrice();
+  res.json(averagePrice);
 });
 
 module.exports = router;
--- controllers/ethersController.js
+++ controllers/ethersController.js
@@ -1,20 +1,87 @@
-const express = require('express');
-const ethersService = require('../services/ethersService');
+/**
+ * @swagger
+ * tags:
+ *   name: Ethers
+ *   description: Ethers API
+ */
+
+const express = require("express");
+const ethersService = require("../services/ethersService");
 
 const router = express.Router();
 
-router.get('/isValidWalletAddress/:address', (req, res) => {
+/**
+ * @swagger
+ * /ethers/isValidWalletAddress/{address}:
+ *   get:
+ *     summary: Check if a wallet address is valid
+ *     tags: [Ethers]
+ *     parameters:
+ *       - in: path
+ *         name: address
+ *         required: true
+ *         description: Wallet address to check
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Indicates whether the address is valid or not
+ *         content:
+ *           application/json:
+ *             example:
+ *               isValid: true
+ *       400:
+ *         description: Invalid wallet address format
+ */
+router.get("/isValidWalletAddress/:address", (req, res) => {
   const { address } = req.params;
   const isValid = ethersService.isValidWalletAddress(address);
   res.json({ isValid });
 });
 
-router.get('/createWallet', (req, res) => {
+/**
+ * @swagger
+ * /ethers/createWallet:
+ *   get:
+ *     summary: Create a new Ethereum wallet
+ *     tags: [Ethers]
+ *     responses:
+ *       200:
+ *         description: Newly created wallet object
+ *         content:
+ *           application/json:
+ *             example:
+ *               address: 0x...
+ *               privateKey: 0x...
+ *       500:
+ *         description: Error occurred while creating the wallet
+ */
+router.get("/createWallet", (req, res) => {
   const wallet = ethersService.createWallet();
   res.json(wallet);
 });
 
-router.get('/getLatestTransactions', async (req, res) => {
+/**
+ * @swagger
+ * /ethers/getLatestTransactions:
+ *   get:
+ *     summary: Get the latest Ethereum transactions
+ *     tags: [Ethers]
+ *     responses:
+ *       200:
+ *         description: Array of latest transactions
+ *         content:
+ *           application/json:
+ *             example:
+ *               - transactionHash: 0x...
+ *                 sender: 0x...
+ *                 receiver: 0x...
+ *                 amount: 1.2345
+ *                 blockNumber: 1234567
+ *       500:
+ *         description: Error occurred while fetching transactions
+ */
+router.get("/getLatestTransactions", async (req, res) => {
   const transactions = await ethersService.getLatestTransactions();
   res.json(transactions);
 });
